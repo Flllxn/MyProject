f664425d9dd732479340ede5fb69273954b4ab86
Flllxn <F.lll.x.n0@gmail.com>
2020-06-18 16:40:50 +0300

    lab11

---
+++ Lab11/lab10/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace lab11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Facade facade = new Facade("Haidamaka Daniil");
+            facade.AddGroup("Group1");
+            facade.AddTask("New one", "Group1");
+        }
+    }
+
+    public class Facade
+    {
+        public DB Obj = new DB();
+        public string Fullname { get; set; }
+
+        public Facade(string fullname)
+        {
+            if(fullname != null)
+            {
+                Fullname = fullname;
+            }
+        }
+
+        public Task AddTask(string name, string groupName)
+        {
+            Obj.Write();
+            return new Task(name, groupName);
+        }
+
+        public Group AddGroup(string name)
+        {
+            Obj.Write();
+            return new Group(name);
+        }
+    }
+
+    public class Task
+    {
+        public string Name { get; set; }
+        public string GroupName { get; set; }
+
+        public Task(string name, string groupName)
+        {
+            if(name != null)
+            {
+                Name = name;
+            }
+
+            if(groupName != null)
+            {
+                GroupName = groupName;
+            }
+
+            Console.WriteLine("Задача создана");
+        }
+
+    }
+
+    public class Group
+    {
+        public string Name { get; set; }
+
+        public Group(string name)
+        {
+            if(name != null)
+            {
+                Name = name;
+            }
+
+            Console.WriteLine("Группа создана");
+        }
+    }
+
+    public class DB
+    {
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string TableName { get; set; }
+
+        public DB()
+        { }
+
+        public string Select()
+        {
+            return "tasks";
+        }
+
+        public void Write()
+        {
+            Console.WriteLine("Writing to DB");
+        }
+    }
+    
+}
